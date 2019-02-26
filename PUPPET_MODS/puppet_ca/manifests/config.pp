c8850509c533a29c863282f6ac418a6fffbe7a06
Abuba-369 <43129621+Abuba-369@users.noreply.github.com>
2019-02-26 17:31:46 +0530

    Create config.pp

---
+++ PUPPET_MODS/puppet_ca/manifests/config.pp
@@ -0,0 +1,49 @@
+# A description of what this class does
+#
+# @summary A short summary of the purpose of this class
+#
+# @example
+#   include puppet_ca::config
+
+class puppet_ca::config (
+
+
+        $ca_server = $::puppet_ca::ca_server,
+        $server   = $::puppet_ca::server,
+) {
+
+        file { '/etc/puppetlabs/puppet/puppet.conf':
+                ensure  => file,
+                mode    => '0644',
+                owner   => 'puppet',
+                group   => 'puppet',
+                content => template('puppet_ca/puppet.conf.erb'),
+                }
+
+         file { '/etc/sysconfig/puppetserver':
+                ensure  => file,
+                mode    => '0644',
+                owner   => 'puppet',
+                group   => 'puppet',
+                source  => 'puppet:///modules/puppet_ca/puppetserver',
+                }
+
+        file { '/etc/puppetlabs/puppet/autosign.conf':
+                ensure  => file,
+                mode    => '0644',
+                owner   => 'puppet',
+                group   => 'puppet',
+                }
+
+
+        file { '/etc/puppetlabs/puppet/':
+                ensure  => directory,
+                mode    => '0644',
+                owner   => 'puppet',
+                group   => 'puppet',
+                owner   => 'puppet',
+                group   => 'puppet',
+                }
+
+
+}
