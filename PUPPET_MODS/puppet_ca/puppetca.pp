d2eafa638912822725f207ae9c3a87c3122e830a
Abuba-369 <43129621+Abuba-369@users.noreply.github.com>
2019-02-26 20:13:34 +0530

    Update puppetca.pp

--- PUPPET_MODS/puppet_ca/puppetca.pp
+++ PUPPET_MODS/puppet_ca/puppetca.pp
@@ -1,4 +1,4 @@
-This file will install puppet CA based on the module puppet_ca
+###This file will install puppet CA based on the module puppet_ca
 class { 'puppet_ca':
         server => hostname,
         ca_server => hostname,
