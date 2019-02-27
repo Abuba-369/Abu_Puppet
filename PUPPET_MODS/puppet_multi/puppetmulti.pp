0cf38fc6b5335e94fe67282916bbc38ee7448ee8
Abuba-369 <43129621+Abuba-369@users.noreply.github.com>
2019-02-27 12:44:44 +0530

    Update puppetmulti.pp

--- PUPPET_MODS/puppet_multi/puppetmulti.pp
+++ PUPPET_MODS/puppet_multi/puppetmulti.pp
@@ -3,5 +3,5 @@
 class { 'puppet_multi':
         server => 'puppetca.zippyops.com',
         ca_server => 'puppetca.zippyops.com',
-        puppetdb_servers => 'server_urls = https://primary-puppetdb.zippyops.com:8081,https://replica-puppetdb.zippyops.com:8081',
+        puppetdb_servers => 'https://primary-puppetdb.zippyops.com:8081,https://replica-puppetdb.zippyops.com:8081',
         }
