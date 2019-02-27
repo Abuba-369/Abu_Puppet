0b601af821df0d3e324a1fdbdf0b6178519f5e25
Abuba-369 <43129621+Abuba-369@users.noreply.github.com>
2019-02-27 12:28:36 +0530

    Update install.pp

--- PUPPET_MODS/puppet_multi/manifests/install.pp
+++ PUPPET_MODS/puppet_multi/manifests/install.pp
@@ -1,8 +1,9 @@
 class puppet_multi::install {
      package { 'puppetserver':
      name   => $::puppet_multi::package,
-  }
+      }
      package { 'puppetdb-termini':
      ensure => present,
      name   => puppetdb-termini,
-  }
+    }
+}
